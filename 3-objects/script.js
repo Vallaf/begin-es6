2f7ef966043d245078d8939b8bc564cd1f092d3f
vl <valerie.lafaiteur@le-campus-numerique.fr>
2019-04-24 14:58:04 +0200

    Object.keys Object.values  Section  1&2

--- 3-objects/script.js
+++ 3-objects/script.js
@@ -12,6 +12,31 @@ const bus = {
     equipments: ['sae', 'tft', 'speech']
 }
 
+//Section  1 : Afficher les carractéristiques du véhicule sours la forme clé : valeur (Ex: id: 3221)
+let tableauBusKeys = Object.keys(bus);
+let tableauBusValues = Object.values(bus);
+
+for (let i = 0; i < tableauBusKeys.length; i++) {
+    console.log(tableauBusKeys[i] + " : " + tableauBusValues[i]);
+}
+
+//Section  1 : Afficher les carractéristiques du véhicule sours la forme clé : valeur (Ex: id: 3221)
+/*for (let key in bus) { 
+    let value = bus [key];
+    console.log(key + " :  " + value)
+    }
+*/
+
 //3.2 Object.values
 
-console.log(data)
+console.log((data));
+
+
+let tableauDataValues = Object.values(data);
+
+for (let i = 0; i < tableauDataValues.length; i++) {
+    let object = tableauDataValues[i];
+    console.log(object.texte);
+}
+
+
